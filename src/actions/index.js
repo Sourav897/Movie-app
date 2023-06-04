17d88165d6c0f77656380ee8410fc11d31e55929
Sourav897 <sauravpandeyaz897@gmail.com>
2023-06-04 16:08:31 +0530

    Add favourites and tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,11 +8,35 @@
 
 // action types
 export const ADD_MOVIES = "ADD_MOVIES";
+export const ADD_FAVOURITES = "ADD_FAVOURITES";
+export const REMOVE_FROM_FAVOURITES = "REMOVE_FROM_FAVOURITES";
+export const SET_SHOW_FAVOURITES = "SET_SHOW_FAVOURITES";
 
-// action creator
+// action creators
 export function addMovies(movies) {
   return {
     type: ADD_MOVIES,
     movies,
+  };
+}
+
+export function addFavourite(movie) {
+  return {
+    type: ADD_FAVOURITES,
+    movie,
+  };
+}
+
+export function removeFromFavourite(movie) {
+  return {
+    type: REMOVE_FROM_FAVOURITES,
+    movie,
+  };
+}
+
+export function setShowFavourite(val) {
+  return {
+    type: SET_SHOW_FAVOURITES,
+    val,
   };
 }
