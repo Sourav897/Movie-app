3813a0a3994f45f94933c54c90cf5f94cd765a12
Sourav897 <sauravpandeyaz897@gmail.com>
2023-06-24 14:04:41 +0530

    using connet() and react-redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,7 +5,8 @@ import {
   ADD_FAVOURITES,
   REMOVE_FROM_FAVOURITES,
   SET_SHOW_FAVOURITES,
-  // ADD_MOVIES_TO_LIST
+  ADD_MOVIES_TO_LIST,
+  ADD_SEARCH_RESULT,
 } from "../actions";
 
 const initialMoviesState = {
@@ -47,11 +48,11 @@ export function movies(state = initialMoviesState, action) {
         ...state,
         showFavourites: action.val,
       };
-    // case ADD_MOVIES_TO_LIST:
-    //   return {
-    //     ...state,
-    //     list: [...action.movie, ...state.list],
-    //   };
+    case ADD_MOVIES_TO_LIST:
+      return {
+        ...state,
+        list: [action.movie, ...state.list],
+      };
     default:
       return state;
   }
@@ -59,10 +60,25 @@ export function movies(state = initialMoviesState, action) {
 
 const initialSearchState = {
   result: {},
+  showSearchResults: false,
 };
 
 export function search(state = initialSearchState, action) {
-  return state;
+  switch (action.type) {
+    case ADD_SEARCH_RESULT:
+      return {
+        ...state,
+        result: action.movie,
+        showSearchResults: true,
+      };
+    case ADD_MOVIES_TO_LIST:
+      return {
+        ...state,
+        showSearchResults: false,
+      };
+    default:
+      return state;
+  }
 }
 
 // const initialRootState = {
